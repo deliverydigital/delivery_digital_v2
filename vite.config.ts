f0b1f7a8fc343a2ed6078f4ee62bfb4628be052b
deliverydigital <zianetransport@gmail.com>
2025-09-04 17:16:07 +0500

    Fix Vite proxy port configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3003',
+        target: 'http://localhost:3008',
         changeOrigin: true,
         secure: false,
       },
