60c70d83ca4ce8e3c5b8a447292290601391134b
deliverydigital <zianetransport@gmail.com>
2025-09-04 19:11:02 +0500

    Updated mongodb.js

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -9,7 +9,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/delivery_d
 // Connection options
 const mongoOptions = {
   maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  serverSelectionTimeoutMS: 15000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
 };
 
