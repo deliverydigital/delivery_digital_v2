6dcc2b49e92bd80984ce7d81c4faaab9affeda38
deliverydigital <zianetransport@gmail.com>
2025-09-04 18:47:33 +0500

    Fix MongoDB connection warnings

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -8,8 +8,6 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/delivery_d
 
 // Connection options
 const mongoOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
