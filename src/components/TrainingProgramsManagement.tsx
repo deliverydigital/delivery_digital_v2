8dd8e1c17af120b7adbf0b301ce40e0cbe0089f1
deliverydigital <zianetransport@gmail.com>
2025-10-01 17:12:27 +0500

    Updated TrainingProgramsManagement.tsx

--- src/components/TrainingProgramsManagement.tsx
+++ src/components/TrainingProgramsManagement.tsx
@@ -550,6 +550,14 @@ const TrainingProgramsManagement = () => {
             </button>
           </div>
         )}
+          {/* Loading state for edit mode */}
+          {modalMode === 'edit' && loadingEditData && (
+            <div className="p-6 text-center">
+              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500 mx-auto mb-4"></div>
+              <p className="text-gray-400">Chargement des données du programme...</p>
+            </div>
+          )}
+
         
         {isLoadingProgramData ? (
           <div className="p-6 text-center">
