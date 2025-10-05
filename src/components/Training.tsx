2291e0c14d22ba4a63af6468e54d38ed7604be72
deliverydigital <zianetransport@gmail.com>
2025-10-05 21:13:44 +0500

    Updated Training.tsx

--- src/components/Training.tsx
+++ src/components/Training.tsx
@@ -215,6 +215,12 @@ const Training = () => {
                       <Users className="h-4 w-4 mr-2" />
                       {t('training.maxParticipants', 'Max {{count}} participants', { count: program.max_participants })}
                     </div>
+                    {program.satisfaction_rate > 0 && (
+                      <div className="flex items-center text-sm text-green-600 font-medium">
+                        <Star className="h-4 w-4 mr-2 fill-current" />
+                        {program.satisfaction_rate}% {t('training.satisfaction', 'Satisfaction')}
+                      </div>
+                    )}
                   </div>
 
                   <div className="flex items-center justify-between">
